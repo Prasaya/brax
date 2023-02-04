349b551101abb9e4978a3fadc41c9b7b000dc168
Prasaya <you@example.com>
2023-02-04 06:49:06 +0000

    Add cmu humanoid

--- brax/envs/__init__.py
+++ brax/envs/__init__.py
@@ -27,6 +27,8 @@ from brax.envs import half_cheetah
 from brax.envs import hopper
 from brax.envs import humanoid
 from brax.envs import humanoid_2 as humanoidNevus
+from brax.envs import humanoid_cmu as humanoidCMU
+
 # from brax.envs import humanoid_new as humanoid
 from brax.envs import humanoid_standup
 from brax.envs import inverted_double_pendulum
@@ -45,90 +47,98 @@ import gym
 print("Using environment init from brax fork")
 
 _envs = {
-    'acrobot': acrobot.Acrobot,
-    'ant': functools.partial(ant.Ant, use_contact_forces=True),
-    'fast': fast.Fast,
-    'fetch': fetch.Fetch,
-    'grasp': grasp.Grasp,
-    'halfcheetah': half_cheetah.Halfcheetah,
-    'hopper': hopper.Hopper,
-    'humanoid': humanoid.Humanoid,
-    'humanoidstandup': humanoid_standup.HumanoidStandup,
-    'inverted_pendulum': inverted_pendulum.InvertedPendulum,
-    'inverted_double_pendulum': inverted_double_pendulum.InvertedDoublePendulum,
-    'pusher': pusher.Pusher,
-    'reacher': reacher.Reacher,
-    'reacherangle': reacherangle.ReacherAngle,
-    'swimmer': swimmer.Swimmer,
-    'ur5e': ur5e.Ur5e,
-    'walker2d': walker2d.Walker2d,
-    'humanoidNevus': humanoidNevus.Humanoid,
-    'mesh': mesh.Mesh
+    "acrobot": acrobot.Acrobot,
+    "ant": functools.partial(ant.Ant, use_contact_forces=True),
+    "fast": fast.Fast,
+    "fetch": fetch.Fetch,
+    "grasp": grasp.Grasp,
+    "halfcheetah": half_cheetah.Halfcheetah,
+    "hopper": hopper.Hopper,
+    "humanoid": humanoid.Humanoid,
+    "humanoidCMU": humanoidCMU.Humanoid,
+    "humanoidstandup": humanoid_standup.HumanoidStandup,
+    "inverted_pendulum": inverted_pendulum.InvertedPendulum,
+    "inverted_double_pendulum": inverted_double_pendulum.InvertedDoublePendulum,
+    "pusher": pusher.Pusher,
+    "reacher": reacher.Reacher,
+    "reacherangle": reacherangle.ReacherAngle,
+    "swimmer": swimmer.Swimmer,
+    "ur5e": ur5e.Ur5e,
+    "walker2d": walker2d.Walker2d,
+    "humanoidNevus": humanoidNevus.Humanoid,
+    "mesh": mesh.Mesh,
 }
 
 
 def get_environment(env_name, **kwargs) -> Env:
-  return _envs[env_name](**kwargs)
+    return _envs[env_name](**kwargs)
 
 
 def register_environment(env_name: str, env_class: Type[Env]):
-  _envs[env_name] = env_class
+    _envs[env_name] = env_class
 
 
-def create(env_name: str,
-           episode_length: int = 1000,
-           action_repeat: int = 1,
-           auto_reset: bool = True,
-           batch_size: Optional[int] = None,
-           eval_metrics: bool = False,
-           **kwargs) -> Env:
-  """Creates an Env with a specified brax system."""
-  env = _envs[env_name](**kwargs)
-  if episode_length is not None:
-    env = wrappers.EpisodeWrapper(env, episode_length, action_repeat)
-  if batch_size:
-    env = wrappers.VectorWrapper(env, batch_size)
-  if auto_reset:
-    env = wrappers.AutoResetWrapper(env)
-  if eval_metrics:
-    env = wrappers.EvalWrapper(env)
+def create(
+    env_name: str,
+    episode_length: int = 1000,
+    action_repeat: int = 1,
+    auto_reset: bool = True,
+    batch_size: Optional[int] = None,
+    eval_metrics: bool = False,
+    **kwargs
+) -> Env:
+    """Creates an Env with a specified brax system."""
+    env = _envs[env_name](**kwargs)
+    if episode_length is not None:
+        env = wrappers.EpisodeWrapper(env, episode_length, action_repeat)
+    if batch_size:
+        env = wrappers.VectorWrapper(env, batch_size)
+    if auto_reset:
+        env = wrappers.AutoResetWrapper(env)
+    if eval_metrics:
+        env = wrappers.EvalWrapper(env)
 
-  return env  # type: ignore
+    return env  # type: ignore
 
 
 def create_fn(env_name: str, **kwargs) -> Callable[..., Env]:
-  """Returns a function that when called, creates an Env."""
-  return functools.partial(create, env_name, **kwargs)
+    """Returns a function that when called, creates an Env."""
+    return functools.partial(create, env_name, **kwargs)
 
 
 @overload
-def create_gym_env(env_name: str,
-                   batch_size: None = None,
-                   seed: int = 0,
-                   backend: Optional[str] = None,
-                   **kwargs) -> gym.Env:
-  ...
+def create_gym_env(
+    env_name: str,
+    batch_size: None = None,
+    seed: int = 0,
+    backend: Optional[str] = None,
+    **kwargs
+) -> gym.Env:
+    ...
 
 
 @overload
-def create_gym_env(env_name: str,
-                   batch_size: int,
-                   seed: int = 0,
-                   backend: Optional[str] = None,
-                   **kwargs) -> gym.vector.VectorEnv:
-  ...
+def create_gym_env(
+    env_name: str,
+    batch_size: int,
+    seed: int = 0,
+    backend: Optional[str] = None,
+    **kwargs
+) -> gym.vector.VectorEnv:
+    ...
 
 
-def create_gym_env(env_name: str,
-                   batch_size: Optional[int] = None,
-                   seed: int = 0,
-                   backend: Optional[str] = None,
-                   **kwargs) -> Union[gym.Env, gym.vector.VectorEnv]:
-  """Creates a `gym.Env` or `gym.vector.VectorEnv` from a Brax environment."""
-  environment = create(env_name=env_name, batch_size=batch_size, **kwargs)
-  if batch_size is None:
-    return wrappers.GymWrapper(environment, seed=seed, backend=backend)
-  if batch_size <= 0:
-    raise ValueError(
-        '`batch_size` should either be None or a positive integer.')
-  return wrappers.VectorGymWrapper(environment, seed=seed, backend=backend)
+def create_gym_env(
+    env_name: str,
+    batch_size: Optional[int] = None,
+    seed: int = 0,
+    backend: Optional[str] = None,
+    **kwargs
+) -> Union[gym.Env, gym.vector.VectorEnv]:
+    """Creates a `gym.Env` or `gym.vector.VectorEnv` from a Brax environment."""
+    environment = create(env_name=env_name, batch_size=batch_size, **kwargs)
+    if batch_size is None:
+        return wrappers.GymWrapper(environment, seed=seed, backend=backend)
+    if batch_size <= 0:
+        raise ValueError("`batch_size` should either be None or a positive integer.")
+    return wrappers.VectorGymWrapper(environment, seed=seed, backend=backend)
